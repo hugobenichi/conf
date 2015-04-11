afebc2e6b8b77ad6bd35e6b8146c645655d74e3c
Hugo Benichi <hugobenichi@gmail.com>
2015-04-11 23:42:34 +0900

    Use absolute path for source dot files in linkdot

--- linkdot.sh
+++ linkdot.sh
@@ -6,13 +6,12 @@
 set -eux
 
 function setup_link {
-  #[ -e $2 ] || echo ln -s $@
   ln -s $@ || true
 }
 
 export -f setup_link
 
 find ./dot* -type f \
-  | awk -v H=$HOME '{print $1, H"/."$1}' \
+  | awk -v P=$(pwd) -v H=$HOME '{print P"/"$1, H"/."$1}' \
   | sed 's|\.\./dot|\.|' \
   | xargs -n 2 bash -c 'setup_link $@' _
